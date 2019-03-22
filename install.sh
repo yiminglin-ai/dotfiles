d7488d29e942168b0b0fc7147b02ee9df79646cf
yl1991 <yiminglin.ee@gmail.com>
2019-03-22 14:15:20 +0000

    ic

--- install.sh
+++ install.sh
@@ -14,5 +14,11 @@ done
 rm -rf ~/.zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+# make pure theme show conda env
+sed -i '190i\\tif [[ -n $CONDA_DEFAULT_ENV ]]; then' ${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/external/pure/pure.zsh
+sed -i '191i\\t\tpsvar[12]="$CONDA_DEFAULT_ENV"' ${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/external/pure/pure.zsh
+sed -i '192i\\tfi' ${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/external/pure/pure.zsh
+
+
 # pull all submodules (vim packages)
 git submodule update --init --recursive
